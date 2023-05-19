1f86d2a978caf9fbb7efe76e9f12d0866af024ff
Ethan <vya9tu@virginia.edu>
2023-05-19 17:39:30 -0400

    w

--- src/Track.js
+++ src/Track.js
@@ -23,7 +23,8 @@ function Track() {
     return (
         <div>
             <h2>{track.name}</h2>
-            <h3>{track.artists}</h3>
+            <h3>{track.artists[0].name}</h3>
+            <img src={track.album.images[0].url} alt={track.name} />
         </div>
     )
 
