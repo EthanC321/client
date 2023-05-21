12935784ab66ac8f2bf6e6fa22cbddf0ed6027f4
Ethan <vya9tu@virginia.edu>
2023-05-21 13:58:00 -0400

    sd

--- src/Album.js
+++ src/Album.js
@@ -46,7 +46,7 @@ function Album() {
             },
             body: JSON.stringify(body)
         })
-        .then(response => console.log(response.json()))
+        .then(response => console.log(response))
 
     }
 
