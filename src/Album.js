e19429dfc35dd06b91ccfa81102de95b72d32b8e
Ethan <vya9tu@virginia.edu>
2023-05-20 21:54:36 -0400

    asdas

--- src/Album.js
+++ src/Album.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
+import './Album.css';
 
 function Album() {
     const urlParams = new URLSearchParams(window.location.search);
@@ -22,22 +23,26 @@ function Album() {
 
     return (
         <div>
-          <button>Add Comment</button>
-          <h2>{album.name}</h2>
-          <img src={album && album.images && album.images[0].url} alt={album.name} width={150} height={150} />
-          {album && album.tracks && album.tracks.items.map((track) => (
-            <React.Fragment key={track.id}>
-              <a href={`/track?q=${track.id}`}>
-                <h3>{track.name}</h3>
-              </a>
-              {track && track.artists.map((artist) => (
-                <h4 key={artist.id}>{artist.name}</h4>
-              ))}
-            </React.Fragment>
-          ))}
+            <button>Add Comment</button>
+            <h2>{album.name}</h2>
+            <img src={album && album.images && album.images[0].url} alt={album.name} width={150} height={150} />
+            <img  id = 'artist' src = {album && album.artists && album.artists?.[0] && album.artists?.[0]?.images[0].url}/>
+            {album && album.tracks && album.tracks.items.map((track) => (
+                <React.Fragment key={track.id}>
+                    <a href={`/track?q=${track.id}`}>
+                        <h3>{track.name}</h3>
+                    </a>
+                    {track && track.artists.map((artist, index) => (
+                        <React.Fragment key={artist.id}>
+                            {index > 0 && ", "} {/* Add comma if it's not the first element */}
+                            <h4>{artist.name}</h4>
+                        </React.Fragment>
+                    ))}
+                </React.Fragment>
+            ))}
         </div>
-      );
-      
+    );
+
 
 
 }
